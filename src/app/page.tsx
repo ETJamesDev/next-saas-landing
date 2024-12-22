e560735c5a2957544d29bea337af80d5358ccd92
Ethan James <ETJames2023@gmail.com>
2024-12-22 01:20:47 -0500

    removed unused image import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 
 export default function Home() {
   return (
